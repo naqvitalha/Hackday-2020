fbb4455b537b2c5a3896ea8296f75ccbaa459db2
naqvitalha <talha.naqvi@flipkart.com>
2020-12-18 02:20:47 +0530

    map method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {BenchRunPage} from "./components/BenchRunPage";
-import { results } from "./data/results";
 import "./styles.css";
-import { mapScore } from "./Util";
 
 export default function App() {
   return (
