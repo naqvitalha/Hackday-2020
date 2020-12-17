83ba2b11da6b35903d2c9f3ae74a86f23d4af082
naqvitalha <talha.naqvi@flipkart.com>
2020-12-18 03:35:26 +0530

    added numbers

--- src/data/results.js
+++ src/data/results.js
@@ -1,14 +1,14 @@
 export const results = {
     cpu : [
         { model: "Pixel 4a", score: 6000 },
-        { model: "OnePlus 5", score: 3000 }
+        { model: "Samsung S10 Lite", score: 3966 }
     ],
     io : [
         { model: "Pixel 4a", score: 6000 },
-        { model: "OnePlus 5", score: 3000 }
+        { model: "Samsung S10 Lite", score: 3911 }
     ],
     webgl: [
         { model: "Pixel 4a", score: 6000 },
-        { model: "OnePlus 5", score: 3000 }
+        { model: "Samsung S10 Lite", score: 354 }
     ],
 }
