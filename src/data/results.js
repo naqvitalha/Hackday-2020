4a96ffe261cdd8236286c20b2f4a161aba7019d3
naqvitalha <talha.naqvi@flipkart.com>
2020-12-18 09:58:31 +0530

    updated phone list

--- src/data/results.js
+++ src/data/results.js
@@ -1,18 +1,27 @@
 export const results = {
     cpu : [
-        { model: "Pixel 4a", score: 6000 },
+        { model: "Pixel 4a", score: 2961 },
         { model: "Samsung S10 Lite", score: 3966 },
-        { model: "ONEPLUS A5000", score: 3000 }
+        { model: "OnePlus 5", score: 1862 },
+        { model: "OnePlus 7T", score: 4125 },
+        { model: "Samsung M30s", score: 1256 }
+
     ],
     io : [
-        { model: "Pixel 4a", score: 6000 },
+        { model: "Pixel 4a", score: 2300 },
         { model: "Samsung S10 Lite", score: 3496 },
-        { model: "ONEPLUS A5000", score: 3000 }
+        { model: "OnePlus 5", score: 2219 },
+        { model: "OnePlus 7T", score: 3233 },
+        { model: "Samsung M30s", score: 1172 }
+
     ],
     webgl: [
-        { model: "Pixel 4a", score: 6000 },
+        { model: "Pixel 4a", score: 246 },
         { model: "Samsung S10 Lite", score: 354 },
-        { model: "ONEPLUS A5000", score: 3000 }
+        { model: "OnePlus 5", score: 83 },
+        { model: "OnePlus 7T", score: 426 },
+        { model: "Samsung M30s", score: 33 }
+
     ],
 }
 
